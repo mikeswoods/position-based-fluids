d4ea79868fc4776628a7e2987754f79697cda00b
Mike Woods <mikeswoods@gmail.com>
2015-04-25 16:05:02 -0700

    Added prefix scan code

---
+++ sim/apps/myApps/pbfSim/src/PrefixSum.cpp
@@ -0,0 +1,9 @@
+//
+//  PrefixSum.cpp
+//  pbfSim
+//
+//  Created by Michael Woods on 4/25/15.
+//
+//
+
+#include "PrefixSum.h"
---
+++ sim/apps/myApps/pbfSim/src/PrefixSum.h
@@ -0,0 +1,14 @@
+//
+//  PrefixSum.h
+//  pbfSim
+//
+//  Created by Michael Woods on 4/25/15.
+//
+//
+
+#ifndef __pbfSim__PrefixSum__
+#define __pbfSim__PrefixSum__
+
+#include <stdio.h>
+
+#endif /* defined(__pbfSim__PrefixSum__) */
